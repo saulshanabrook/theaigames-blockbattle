0079f81bfa53d2a54ff5f934ebe57f04ae6b0790
Saul Shanabrook <s.shanabrook@gmail.com>
2016-05-05 16:09:41 -0400

    cleanup tail process  after they exit

--- rl/engine/pipe.bash
+++ rl/engine/pipe.bash
@@ -7,5 +7,9 @@
 READ_FROM="$1"
 WRITE_TO="$2"
 
+# so that tail is cleaned up
+# http://stackoverflow.com/a/2173421/907060
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 tail -f $READ_FROM &
 cat /dev/stdin > $WRITE_TO
